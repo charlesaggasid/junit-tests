29c0faf9025c080dcf51cd7b7571f52581ab69c5
Charles Aggasid <charles.aggasid01@gmail.com>
2022-01-21 14:24:32 -0600

    added studen

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,3 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
 public class StudentTest {
+    @Test
+    public void testNewStudent(){
+        Student s1 = new Student(1l, "Charles"); // create new obj
+        Student charles = null;
+        assertNull(charles); // checking if null is instantiated.
+        assertNotNull(s1); //checking if s1 is not instantiated.
+    }
+
+    @Test
+    public void testAddGrade(){
+        Student s1 = new Student(1L, "Charles"); //create new obj
+        s1.addGrade(90); // adding the grade
+        assertSame(90, s1.getGrades().get(0)); //With assertNotSame, it's possible to verify if two variables don't refer to the same object. Otherwise, when we want to verify that two variables refer to the same object, we can use the assertSame assertion.
+    }
+
+    @Test
+    public void testAverageGrade(){
+        Student s1 = new Student(1L, "Charles");
+        s1.addGrade(90);
+        s1.addGrade(80);
+        s1.addGrade(95);
+        
+    }
+
+
+
 
 }
